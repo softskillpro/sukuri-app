69b951787e59076ef82c600b3eed36e76699bd08
fullDev9233 <jimyy9211@gmail.com>
2023-06-23 11:35:22 -0500

    feature/SPK-142

--- src/components/Subscription/index.tsx
+++ src/components/Subscription/index.tsx
@@ -1,35 +1,73 @@
-import { useCallback } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import { toast } from 'react-toastify';
 import Typography from '@mui/material/Typography';
 import { HorizontalDivider } from '@/components/common/StyledDivider';
 import { Layout1, Layout2 } from '@/components/common/PageLayout';
 import TierCard from '@/components/common/TierCard';
 import FlexBox from '@/components/common/FlexBox';
 import type { NFTType } from '@/interface/Nft.interface';
-import axios from 'axios'; // import axios to make HTTP requests
+import axios from 'axios';
 
 interface SubscriptionProps {
-  nft?: NFTType;
+  project?: NFTType;
 }
 
-const Subscription = ({ nft }: SubscriptionProps) => {
-  const handleClick = useCallback((tierId: number | undefined) => {
-    // Make a POST request to your subscription API
-    if (!tierId) {
-      return null
-    }
-    const tierIdNum = Number(tierId);
-    console.log(tierIdNum);
-    axios.post('/api/subscribe', {
-      projectId: nft?.id,
-      tierId: tierIdNum
-    }).then((response) => {
-      console.log(response.data); // log the server response
-    }).catch((error) => {
-      console.error(error); // log any error
-    });
-  }, [nft?.id]);
+const Subscription = ({ project }: SubscriptionProps) => {
+  const [loading, setLoading] = useState(false);
+  const [isSubscribed, setIsSubscribed] = useState(false);
 
-  if (!nft) return null;
+  useEffect(() => {
+    const fetchData = async (_projectId: string) => {
+      try {
+        const response = await fetch(`/api/subscribe?projectId=${_projectId}`);
+        if (!response.ok) {
+          throw new Error('Request failed');
+        }
+        const data = await response.json();
+        setIsSubscribed(data.isSubscribed);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (project) {
+      fetchData(project.id);
+    }
+  }, [project]);
+
+  const handleClick = useCallback(
+    (tierId: number | undefined) => {
+      // Make a POST request to your subscription API
+      if (!tierId) return;
+
+      const tierIdNum = Number(tierId);
+
+      setLoading(true);
+
+      const subscription = axios
+        .post('/api/subscribe', {
+          projectId: project?.id,
+          tierId: tierIdNum,
+        })
+        .then((response) => {
+          setIsSubscribed(true);
+          console.log(response.data); // log the server response
+        })
+        .catch((error) => {
+          console.error(error); // log any error
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+
+      toast.promise(subscription, {
+        pending: 'Processing now...',
+        success: 'Subscription completed successfully!',
+        error: 'Subscription failed!',
+      });
+    },
+    [project?.id],
+  );
 
   return (
     <>
@@ -43,13 +81,18 @@ const Subscription = ({ nft }: SubscriptionProps) => {
 
       <Layout1 sx={{ mb: 6.25 }}>
         <FlexBox sx={{ justifyContent: 'center', flexWrap: 'wrap', gap: 2.5 }}>
-          {
-            nft.tiers.map((tier) => (
+          {project &&
+            project.tiers.map((tier) =>
               tier.id ? (
-                <TierCard key={tier.id} tier={tier} handleClick={() => handleClick(tier.id)} />
-              ) : null
-            ))
-          }
+                <TierCard
+                  key={tier.id}
+                  tier={tier}
+                  loading={loading}
+                  isSubscribed={isSubscribed}
+                  handleClick={() => handleClick(tier.id)}
+                />
+              ) : null,
+            )}
         </FlexBox>
       </Layout1>
     </>
