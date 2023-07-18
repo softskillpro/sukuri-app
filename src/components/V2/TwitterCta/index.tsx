a7e19f7ae466a455604d41db32a2b3fe484e57f2
spuro <36612221+spuro@users.noreply.github.com>
2023-07-18 16:19:16 +0930

    page layout, category grid, AboutSukuri

---
+++ src/components/V2/TwitterCta/index.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image';
+import Typography from '@mui/material/Typography';
+
+import { TwitterCTAContainer } from './styles';
+
+const TwitterCTA = () => {
+  return (
+    <TwitterCTAContainer>
+      
+    </TwitterCTAContainer>
+  );
+};
+
+export default TwitterCTA;
