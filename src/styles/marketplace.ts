adbd0689baab8ec0e33ca903eb9ac1c579ffd785
spuro <36612221+spuro@users.noreply.github.com>
2023-07-18 23:24:25 +0930

    md screen input width

--- src/styles/marketplace.ts
+++ src/styles/marketplace.ts
@@ -35,7 +35,9 @@ export const MarketplaceContainer = styled(Box)(({ theme }) => ({
       border: "1px solid rgba(255,255,255,1)",
       outline: 'none',
     },
-    
+    [theme.breakpoints.down('md')]: {
+      width: "80%",
+    }
   }
 }));
 
