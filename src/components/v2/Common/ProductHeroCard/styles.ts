5314972e8559a905af5ea77dc9774507db50e749
spuro <36612221+spuro@users.noreply.github.com>
2023-07-19 14:31:23 +0930

    productHero details

--- src/components/v2/Common/ProductHeroCard/styles.ts
+++ src/components/v2/Common/ProductHeroCard/styles.ts
@@ -2,18 +2,24 @@ import { styled } from '@mui/material/styles';
 
 export const ProductHeroCardContainer = styled('div')(({ theme }) => ({
   position: 'relative',
-  borderRadius: 5,
+  borderRadius: 15,
   background:
     'linear-gradient(270deg, #000 0%, rgba(0, 0, 0, 0.72) 28.13%, rgba(0, 0, 0, 0.00) 100%)',
   zIndex: 50,
+  margin: "0px 15px",
+  boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.6)',
 
   '.product-hero-img': {
     position: 'absolute',
     width: '100%',
     height: '100%',
-    borderRadius: 5,
+    borderRadius: 15,
     zIndex: -5,
     objectFit: 'cover',
+
+    [theme.breakpoints.down('md')]: {
+      borderRadius: 10,
+    }
   },
 
   '.product-hero=img::after': {
@@ -57,6 +63,9 @@ export const ProductHeroCardContainer = styled('div')(({ theme }) => ({
   },
 
   [theme.breakpoints.down('md')]: {
+
+    borderRadius: 10,
+
     '.product-hero-card-wrapper': {
       '.product-hero-content-wrapper': {
         marginBottom: 60,
