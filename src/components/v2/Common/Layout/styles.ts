ec15d34494d865b93cb07a9239329776a7107934
fullDev9233 <jimyy9211@gmail.com>
2023-07-24 12:12:56 -0500

    fix/set-max-width

--- src/components/v2/Common/Layout/styles.ts
+++ src/components/v2/Common/Layout/styles.ts
@@ -1,8 +1,9 @@
-import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
+import FlexBox from '@/components/v2/Common/FlexBox';
 
-export const LayoutContainer = styled(Box)(({ theme }) => ({
+export const LayoutContainer = styled(FlexBox)(({ theme }) => ({
   position: 'relative',
+  flexDirection: 'column',
   overflowX: 'hidden',
   width: '100%',
   height: 'inherit',
@@ -20,13 +21,14 @@ export const LayoutContainer = styled(Box)(({ theme }) => ({
     height: '100%',
     zIndex: 1,
     mixBlendMode: 'lighten',
-    objectFit: 'cover', 
-    pointerEvents: 'none'
+    objectFit: 'cover',
+    pointerEvents: 'none',
   },
 
   '.main-wrapper': {
     position: 'inherit',
     width: '100%',
+    maxWidth: 1440,
     height: 'inherit',
     minHeight: '100vh',
     zIndex: 10,
