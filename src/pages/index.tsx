3cf8bf709775f7679033f21b7f32a4d182f5d2b7
M4cs <me@maxbridgland.com>
2023-10-11 16:34:29 -0400

    Rename files to hide pages a bit better

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,70 +1,109 @@
-import { useCallback, useEffect, useState } from 'react';
-import MarketplaceHero from '@/components/MarketplaceHero';
-import CategorySearch from '@/components/CategorySearch';
-import FeaturedProducts from '@/components/FeaturedProducts';
-import NewProducts from '@/components/NewProducts';
-import Explorer from '@/components/Explorer';
-import IntroSukuri from '@/components/IntroSukuri';
-import BecomeAffiliate from '@/components/BecomeAffiliate';
+import { useEffect } from 'react';
+import Image from 'next/image';
+import { Typography, useMediaQuery, useTheme } from '@mui/material';
+import MintHero from '@/components/MintHero';
+import { FlexBox } from '@/components/Common/FlexBox';
+import { Section } from '@/components/Common/Section';
 import Newsletter from '@/components/Newsletter';
-import { MarketplaceContainer } from '@/styles/home';
+import { MintContainer } from '@/styles/mint';
 import useRuntimeContext from '@/hooks/useRuntimeContext';
-import { api } from '@/utils/api';
-import { currencies } from '@/constants';
 
-const Marketplace = () => {
-  const { data: allProducts, status }: any = api.project.get.useQuery({
-    asc: true,
-  });
-
+const Mint = () => {
   const { fetchHandler } = useRuntimeContext();
 
-  const [currency, setCurrency] = useState(currencies[0] || 'USD');
-  const [search, setSearch] = useState<string | undefined>(undefined);
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up('md'));
 
   useEffect(() => {
     fetchHandler(false);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const handleCurrencyChange = useCallback((_currency: string) => {
-    setCurrency(_currency);
-  }, []);
-
-  const handleSearch = useCallback((_search?: string) => {
-    setSearch(_search);
-  }, []);
-
   return (
-    <MarketplaceContainer>
-      <MarketplaceHero
-        currency={currency}
-        handleCurrencyChange={handleCurrencyChange}
-      />
+    <MintContainer>
+      <MintHero />
 
-      <CategorySearch handleSearch={handleSearch} />
+      <Section className='mint-grid refer-friends'>
+        <FlexBox className='mint-contents'>
+          <Typography variant={matches ? 'h3' : 'h3Mobile'}>
+            Refer Your Friends
+          </Typography>
 
-      <FeaturedProducts
-        loading={status === 'loading'}
-        products={allProducts}
-        currency={currency}
-      />
+          <Typography
+            variant={matches ? 'body1' : 'body2Mobile'}
+            color='accent.light'
+          >
+            Your display name becomes your referral code. 5% of every purchase
+            made with your code will go back to you. An additional 0.5% will be
+            earned through all secondary referrals. In addition to Ether, you
+            will also earn points for all referrals you make! Leaderboard coming
+            soon!
+          </Typography>
+        </FlexBox>
 
-      <NewProducts
-        loading={status === 'loading'}
-        products={allProducts}
-        currency={currency}
-      />
+        <Image
+          src='/images/mint-4.png'
+          width={300}
+          height={300}
+          alt='First To Know'
+          className='mint-asset mint-asset3'
+        />
+      </Section>
 
-      <Explorer />
+      <Section className='mint-grid'>
+        <Image
+          src='/images/mint-3.png'
+          width={300}
+          height={300}
+          alt='First To Know'
+          className='mint-asset mint-asset2'
+        />
 
-      <IntroSukuri />
+        <FlexBox className='mint-contents'>
+          <Typography variant={matches ? 'h3' : 'h3Mobile'}>
+            Earn rewards!
+          </Typography>
 
-      <BecomeAffiliate />
+          <Typography
+            variant={matches ? 'body1' : 'body2Mobile'}
+            color='accent.light'
+          >
+            Once you have purchased a Prime Pass, complete quests, provide
+            feedback and earn badges that are dynamically added to your pass.
+            Collect all the badges to get access to prizes down the road!
+          </Typography>
+        </FlexBox>
+      </Section>
 
-      <Newsletter />
-    </MarketplaceContainer>
+      <Section className='mint-grid'>
+        <FlexBox className='mint-contents'>
+          <Typography variant={matches ? 'h3' : 'h3Mobile'}>
+            Be the first to know...
+          </Typography>
+
+          <Typography
+            variant={matches ? 'body1' : 'body2Mobile'}
+            color='accent.light'
+          >
+            As a valued early supporter, you will be the first to see updates to
+            our protocol as we build in public. Enjoy access to a private
+            channel in our discord where you can chat with the founders and
+            receive that sweet, sweet alpha.
+          </Typography>
+        </FlexBox>
+
+        <Image
+          src='/images/mint-2.png'
+          width={300}
+          height={300}
+          alt='First To Know'
+          className='mint-asset mint-asset1'
+        />
+      </Section>
+
+      <Newsletter className='mint-newsletter' />
+    </MintContainer>
   );
 };
 
-export default Marketplace;
+export default Mint;
